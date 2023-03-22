3db2fdc06fac67deafa9b1c6f56cb4939f9e4489
Levi Garcia <58457288+levigg4@users.noreply.github.com>
2023-03-22 09:45:54 -0600

    Add files via upload

---
+++ EVA_18_INTERFACES_HERENCIA/src/eva_18_interfaces_herencia/EVA_18_INTERFACES_HERENCIA.java
@@ -0,0 +1,42 @@
+package eva_18_interfaces_herencia;
+//RODRIGO LEVI GARCIA GARAY
+
+public class EVA_18_INTERFACES_HERENCIA {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
+
+interface intA {
+    void A();
+}
+
+interface intB {
+    void B();
+}
+
+interface intC extends intA, intB{
+    void C();
+}
+
+class Prueba implements intC{
+
+    @Override
+    public void C() {
+        
+    }
+
+    @Override
+    public void A() {
+        
+    }
+
+    @Override
+    public void B() {
+        
+    }
+
+
+}
